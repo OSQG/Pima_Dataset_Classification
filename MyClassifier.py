7666989dc47642bf42df01ff7c319fd40bab129c
Abdullah Safi <asaf@uni.sydney.edu.au>
2021-05-10 16:33:08 +1000

    fixing error in dataset + knn file created

--- MyClassifier.py
+++ MyClassifier.py
@@ -1,7 +1,10 @@
+import sys
+import numpy
+from algorithms.KNearestNeighbour import KNN
+
 
 
 if __name__ == "__main__":
     training_filename = sys.argv[1]
     testing_filename = sys.argv[2]
     classifier = sys.argv[3]
-    f = open(file_name, "r")
