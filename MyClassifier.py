f17c54ff5a09ad0b8818720866c41bb31f6b7542
Abdullah Safi <asaf@uni.sydney.edu.au>
2021-05-11 19:09:49 +1000

    knn done

--- MyClassifier.py
+++ MyClassifier.py
@@ -3,8 +3,6 @@ import numpy
 from algorithms.KNearestNeighbour import KNN
 from algorithms.NaiveBayes import NaiveBayes
 
-
-
 if __name__ == "__main__":
     training_filename = sys.argv[1]
     testing_filename = sys.argv[2]
@@ -14,11 +12,17 @@ if __name__ == "__main__":
         data = []
         f = open(filepath, "r")
         lines = f.readlines()
-        for i in lines:
-            data.append(i.strip("\n").split(","))
+        for line in lines:
+            line = line.strip("\n").split(",")
+            for i in range(0, len(line)):
+                # Convert numeric values into float
+                if line[i] != "yes" and line[i] != "no":
+                    line[i] = float(line[i])
+            data.append(line)
+        f.close()
         return data
 
     if classifier == "NB":
         NaiveBayes(None, None)
     elif classifier[-2:] == "NN":
-        KNN(classifier[:-2], read_Data(testing_filename), read_Data(training_filename))
+        KNN(int(classifier[:-2]), read_Data(testing_filename), read_Data(training_filename))
--- algorithms/KNearestNeighbour.py
+++ algorithms/KNearestNeighbour.py
@@ -1,2 +1,43 @@
+from math import sqrt
+
+# Calculate the Euclidean distance between two vectors
+def euclidean_distance(vector1, vector2):
+	distance = 0.0
+	for i in range(len(vector1)-1):
+		distance += (vector1[i] - vector2[i])**2
+	return sqrt(distance)
+
+# Get k nearest neighbours
+def get_neighbours(k, test_instance, training):
+    distances_sorted = []
+    for training_row in training:
+        # Store training row and dist as a tuple to sort later
+        dist = euclidean_distance(test_instance, training_row)
+        distances_sorted.append((training_row, dist))
+    # Sort the distances
+    distances_sorted.sort(key=lambda tup: tup[1])
+    # Get k nearest neigbours
+    neighbours = []
+    for i in range(k):
+        # Get the k closet rows
+        neighbours.append(distances_sorted[i][0])
+    return neighbours
+
 def KNN(k, testing, training):
-    return
+    predictions = []
+    for testing_row in testing:
+        k_neighbours = get_neighbours(k, testing_row, training)
+        yes = 0
+        no = 0
+        for i in k_neighbours:
+            if i[-1] == "yes":
+                yes = yes + 1
+            elif i[-1] == "no":
+                no = no + 1
+        if no > yes:
+            predictions.append("no")
+        else:
+            predictions.append("yes")
+    for i in predictions:
+        print(i)
+    return predictions
