d2ba17d9547e45c9a5ebf56cf802036e34a9e9ba
Abdullah Safi <asaf@uni.sydney.edu.au>
2021-05-14 23:33:32 +1000

    edits on cv fold + report added

--- cv_10fold.py
+++ cv_10fold.py
@@ -23,28 +23,25 @@ def read_Data(filepath):
 
 # Creates 10 stratified folds with the data
 def create_stratified_folds(yes_instances, no_instances):
-    folds = dict()
+    stratified_folds = dict()
     for i in range(10):
-        folds[i] = []
-    i = 0
-    while len(yes_instances) > 0:
-        folds[i % 10].append(yes_instances.pop())
-        i += 1
+        stratified_folds[i] = []
+    partition = 0
     while len(no_instances) > 0:
-        folds[i % 10].append(no_instances.pop())
-        i += 1
-    # for i in range(10):
-    #     print(len(folds[i]))
-    return folds
+        partition += 1
+        stratified_folds[partition % 10].append(no_instances.pop())
+    while len(yes_instances) > 0:
+        partition += 1
+        stratified_folds[partition % 10].append(yes_instances.pop())
+    return stratified_folds
 
 def ten_fold_cv(folds):
-    one_nn_accuracy = []
-    five_nn_accuracy = []
+    oneNN_accuracy = []
+    fiveNN_accuracy = []
     nb_accuracy = []
     # print("YOOOO")
     # print(folds[0])
     for i in range(10):
-        validation_instances = list(folds[i])
         validation_instances = []
         for row_number in range(len(folds[i])):
             validation_instances.append([])
@@ -66,37 +63,38 @@ def ten_fold_cv(folds):
             del row[-1]
         
         # print(folds[0])
-        one_nn_results = KNN(1, validation_instances, training_instances)
-        five_nn_results = KNN(5, validation_instances, training_instances)
+        oneNN_results = KNN(1, validation_instances, training_instances)
+        fiveNN_results = KNN(5, validation_instances, training_instances)
         nb_results = NaiveBayes(validation_instances, training_instances) # fails due to length of yes and no =0
         
 
-        nb_score = 0
-        one_nn_score = 0
-        five_nn_score = 0
+        nb_correct = 0
+        oneNN_correct = 0
+        fiveNN_correct = 0
         for k in range(len(validation_instances)):
-            if one_nn_results[k] == validation_outcomes[k]:
-                one_nn_score += 1
-            if five_nn_results[k] == validation_outcomes[k]:
-                five_nn_score += 1
+            if oneNN_results[k] == validation_outcomes[k]:
+                oneNN_correct += 1
+            if fiveNN_results[k] == validation_outcomes[k]:
+                fiveNN_correct += 1
             if nb_results[k] == validation_outcomes[k]:
-                nb_score += 1
-        nb_accuracy.append(nb_score/len(validation_instances))
-        one_nn_accuracy.append(one_nn_score/len(validation_instances))
-        five_nn_accuracy.append(five_nn_score / len(validation_instances))
-        print('Round {} accuracy for 1-Nearest Neighbours: {}'.format(i + 1, one_nn_accuracy[i]))
-        print('Round {} accuracy for 5-Nearest Neighbours: {}'.format(i + 1, five_nn_accuracy[i]))
-        print('Round {} accuracy for Naive Bayes: {}'.format(i+1, nb_accuracy[i]))
+                nb_correct += 1
+        
+        oneNN_accuracy.append(oneNN_correct/len(validation_instances))
+        fiveNN_accuracy.append(fiveNN_correct / len(validation_instances))
+        nb_accuracy.append(nb_correct/len(validation_instances))
 
-    nb_average_acc = numpy.mean(nb_accuracy)
-    one_nn_average_acc = numpy.mean(one_nn_accuracy)
-    five_nn_average_acc = numpy.mean(five_nn_accuracy)
+        print('Round {} accuracy for 1-Nearest Neighbours: {}'.format(i + 1, oneNN_accuracy[i]))
+        print('Round {} accuracy for 5-Nearest Neighbours: {}'.format(i + 1, fiveNN_accuracy[i]))
+        print('Round {} accuracy for Naive Bayes: {}'.format(i + 1, nb_accuracy[i]))
+
+    oneNN_average = numpy.mean(oneNN_accuracy)
+    fiveNN_average = numpy.mean(fiveNN_accuracy)
+    nb_average = numpy.mean(nb_accuracy)
+
     print(" ")
-    print('Average accuracy for 1-Nearest Neighbours: {}%'.format(one_nn_average_acc))
-    print('Average accuracy for 5-Nearest Neighbours: {}%'.format(five_nn_average_acc))
-    print('Average accuracy for Naive Bayes: {}%'.format(nb_average_acc))
-
-
+    print('Average accuracy for 1-Nearest Neighbours: {}%'.format(oneNN_average))
+    print('Average accuracy for 5-Nearest Neighbours: {}%'.format(fiveNN_average))
+    print('Average accuracy for Naive Bayes: {}%'.format(nb_average))
 
 def write_output_file(folds):
     with open('pima-folds.csv', 'w') as outfile:
@@ -113,8 +111,6 @@ def write_output_file(folds):
 if __name__ == "__main__":
     data_file = sys.argv[1]
     yes_instances, no_instances = read_Data(data_file)
-    # print(yes_instances) # yes and no kept
     stratified_folds = create_stratified_folds(yes_instances, no_instances)
-    # print(stratified_folds) # yes and no are still here
     #write_output_file(stratified_folds)
     ten_fold_cv(stratified_folds)
